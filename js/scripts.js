19271b98f5fd8d09ad46e3d30c1f7485d15aef21
Nathan Butler <iamnbutler(at)gmail.com>
2012-09-29 01:41:33 -0400

    Re-added jQuery files

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,6 @@
+/* Based off of ENSO 1.0 / A minimal HTML5 boilerplate - Spiegelma.de */
+
 // Load scripts after page loads
 $(document).ready(function(){
-	$('#sidebar-nav li').click(function(){
-		$('#sidebar-nav li').removeClass('tier-one-selected');
-		$(this).addClass('tier-one-selected');
-		$('#sidebar-nav li ul li').hide();
-		$(this).find('li').each(function(index) {
-		    $(this).delay(400*index).fadeIn(200);
-		});
-		return false;
-	});
+
 });
